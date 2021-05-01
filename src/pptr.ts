028992be787e0336d507a763123e95d3eb031d24
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-01 17:58:49 +0900

    refactor: main.ts から pptr 分離 (#58)
    * separate browser
    
    * separate dakoku
    
    * to arrow func
    
    * move getPage
    
    * remove getBrowser
    
    * summarize func
    
    * Revert "summarize func"
    
    This reverts commit 26235fbcf21b01c2086c79d8074587e235851825.
    
    * Revert "remove getBrowser"
    
    This reverts commit e80ab417d141419a7cfd8df6a6f6ef7792b63259.
    
    * Revert "move getPage"
    
    This reverts commit bb978b07ac12d9c81e6486e2f3971b23221a4bd3.
    
    * Revert "separate dakoku"
    
    This reverts commit 0c2dc9ececf98cbda61d862e098049c92949c353.
    
    * separate getPage
    
    * rename
    
    * rename

---
+++ src/pptr.ts
@@ -0,0 +1,16 @@
+import { App, BrowserWindow } from 'electron';
+import { Browser, Page } from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import pie from 'puppeteer-in-electron';
+
+let browser: Browser | null = null;
+
+export const initialize = async (app: App, port: number): Promise<void> => {
+  await pie.initialize(app, port);
+  browser = await pie.connect(app, puppeteer);
+};
+
+export const getPage = (win: BrowserWindow): Promise<Page> => {
+  if (!browser) throw new Error('browser is not initialized.');
+  return pie.getPage(browser, win);
+};
