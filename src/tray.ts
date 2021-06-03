1c4f6beca6fac86085574e872871d48dac15148c
Naoki Ohzeki <0h23k1.n40k1@gmail.com>
2021-06-04 00:28:33 +0900

    refactor: generateMenu関数をリファクタ (#99)
    * Refactor generateMenu.
    
    * Update src/tray.ts
    
    Co-authored-by: Amon <amon.sawamura@gmail.com>
    
    * Remove conditionTuple.
    
    * Use reduce insteadof filter and map.
    
    * Revert "Use reduce insteadof filter and map."
    
    This reverts commit 0e8550f922f6658e48f68720c45d31ded387d1bf.
    
    * Remove unused variables.
    
    * Update src/tray.ts
    
    Co-authored-by: Amon <amon.sawamura@gmail.com>
    
    Co-authored-by: Amon <amon.sawamura@gmail.com>

--- src/tray.ts
+++ src/tray.ts
@@ -12,112 +12,68 @@ const getIcon = (): string => {
 };
 const icon = getIcon();
 
+const generateMenuItem = (
+  type: 'normal' | 'separator',
+  label?: string,
+  click?: (menuItem: MenuItem) => void
+): MenuItemConstructorOptions => {
+  return {
+    type,
+    label,
+    click,
+  };
+};
+
 const generateMenu = (
   open: () => void,
   run: (task: TaskType) => Promise<void>,
   showDirectly: boolean,
-  release: StoreRelease | null
+  storeRelease: StoreRelease | null
 ): (MenuItemConstructorOptions | MenuItem)[] => {
-  const menu: (MenuItemConstructorOptions | MenuItem)[] = [];
-
-  menu.push(
-    {
-      type: 'normal',
-      label: '出勤打刻',
-      click: () => {
-        run('startWork');
-      },
-    },
-    {
-      type: 'normal',
-      label: '出勤打刻・テレワーク開始',
-      click: () => {
-        run('startTelework');
-      },
-    },
-    {
-      type: 'normal',
-      label: '退勤打刻',
-      click: () => {
-        run('finishWork');
-      },
-    },
-    {
-      type: 'separator',
-    }
-  );
-
-  if (showDirectly) {
-    menu.push(
-      {
-        type: 'normal',
-        label: '直行打刻',
-        click: () => {
-          run('startWorkDirectly');
-        },
-      },
-      {
-        type: 'normal',
-        label: '直帰打刻',
-        click: () => {
-          run('finishWorkDirectly');
-        },
-      },
-      {
-        type: 'separator',
-      }
-    );
-  }
-
-  menu.push(
-    {
-      type: 'normal',
-      label: '私用外出開始',
-      click: () => {
-        run('pauseWork');
-      },
-    },
-    {
-      type: 'normal',
-      label: '私用外出終了',
-      click: () => {
-        run('restartWork');
-      },
-    },
-    {
-      type: 'separator',
-    },
-    {
-      type: 'normal',
-      label: 'ログイン・設定',
-      click: open,
-    },
-    {
-      type: 'separator',
-    },
-    {
-      type: 'normal',
-      label: 'AKASHI',
-      click: () => {
-        shell.openExternal('https://atnd.ak4.jp/login');
-      },
-    }
-  );
-  if (release) {
-    menu.push({
-      type: 'normal',
-      label: `🌟DakokuApp: ${release.name}を入手する🌟`,
-      click: () => {
-        shell.openExternal(release.html_url);
-      },
-    });
-  }
-  menu.push({
-    type: 'normal',
-    label: '終了',
-    click: app.quit,
+  const separator = generateMenuItem('separator');
+  const startWork = generateMenuItem('normal', '出勤打刻', () => {
+    run('startWork');
   });
-
+  const finishWork = generateMenuItem('normal', '退勤打刻', () => {
+    run('finishWork');
+  });
+  const startWorkDirectly = generateMenuItem('normal', '直行打刻', () => {
+    run('startWorkDirectly');
+  });
+  const finishWorkDirectly = generateMenuItem('normal', '直帰打刻', () => {
+    run('finishWorkDirectly');
+  });
+  const pauseWork = generateMenuItem('normal', '私用外出開始', () => {
+    run('pauseWork');
+  });
+  const restartWork = generateMenuItem('normal', '私用外出終了', () => {
+    run('restartWork');
+  });
+  const loginAndSetting = generateMenuItem('normal', 'ログイン・設定', open);
+  const akashi = generateMenuItem('normal', 'AKASHI', () => {
+    shell.openExternal('https://atnd.ak4.jp/login');
+  });
+  const release = generateMenuItem('normal', `🌟DakokuApp: ${storeRelease?.name}を入手する🌟`, () => {
+    shell.openExternal(storeRelease?.html_url + '');
+  });
+  const quit = generateMenuItem('normal', '終了', app.quit);
+  const itemAndCondition: [MenuItemConstructorOptions, boolean][] = [
+    [startWork, true],
+    [finishWork, true],
+    [separator, true],
+    [startWorkDirectly, showDirectly],
+    [finishWorkDirectly, showDirectly],
+    [separator, showDirectly],
+    [pauseWork, true],
+    [restartWork, true],
+    [separator, true],
+    [loginAndSetting, true],
+    [separator, true],
+    [akashi, true],
+    [release, storeRelease !== null],
+    [quit, true],
+  ];
+  const menu = itemAndCondition.filter(([, condition]) => condition).map(([item]) => item);
   return menu;
 };
 
