20c957416783b716f038166027c256ef528e60d6
zeek0x <0h23k1.n40k1@gmail.com>
2021-06-20 00:36:11 +0900

    Rename var.

--- src/resource.ts
+++ src/resource.ts
@@ -3,11 +3,11 @@ import path from 'path';
 type Resource = 'icon' | 'trayIcon';
 
 export const getPath = (resource: Resource): string => {
-  const iconPath = {
+  const resourcePath = {
     icon: 'img/icon.png',
     trayIcon: 'img/TrayIconTemplate.png',
   }[resource];
   return process.env.NODE_ENV !== 'development'
-    ? path.join(process.resourcesPath, iconPath)
-    : path.join(__dirname, '..', iconPath);
+    ? path.join(process.resourcesPath, resourcePath)
+    : path.join(__dirname, '..', resourcePath);
 };
